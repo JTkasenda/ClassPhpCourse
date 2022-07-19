4e73d5d51ba64efa3a0789df01611cb89c956c58
JTkasenda <jeantshimbalanga9@gmail.com>
2022-07-19 20:34:15 +0100

    classwork

--- html/super.js
+++ html/super.js
@@ -1,21 +1,39 @@
 
 document.getElementById("Pu1").value = "500";
+document.getElementById("Pu2").value = "500";
+document.getElementById("Pu3").value = "500";
+function Produit(produit, prix){
 
-function Produit(){
-
-    var P1 = document.getElementById("Produit1").value;
-    
+    var P1 = document.getElementById(produit).value;    
     if(P1=="Ordinateur"){
-        document.getElementById("Pu1").value = "500";
+        document.getElementById(prix).value = "500";
     }
     else if(P1=="Telephone"){
-        document.getElementById("Pu1").value = "300";
+        document.getElementById(prix).value = "300";
     }
     else if(P1=="Pain au lait"){
-        document.getElementById("Pu1").value = "2000";
+        document.getElementById(prix).value = "2000";
     }
     else{
-        document.getElementById("Pu1").value = "500";
+
+        document.getElementById(prix).value = "500";
     }
 }
 
+function PrixTotal(qte, prix, ptotal){
+
+    var Qte1 = document.getElementById(qte).value
+    var Price = document.getElementById(prix).value;
+    document.getElementById(ptotal).value = Number(Qte1 * Price);
+}
+
+function sum(){
+    
+    var price1 = document.getElementById("pt1");
+    var price2 = document.getElementById("pt2");
+    var price3 = document.getElementById("pt3");
+
+    var total = Number(price1 + price2 + price3);
+
+    document.getElementById("PTHT").value = total;
+}
